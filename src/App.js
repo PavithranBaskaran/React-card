83963ebdf36602adc7203b6a8ca94a13ac7046b0
PavithranBaskaran <vktpavi@gmail.com>
2022-06-16 17:38:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Card from "./Card";
+let details = [
+  {
+    title: "Free",
+    price: "$0",
+    feature: [
+      { list: "Single User", class: "" },
+      { list: "5GB Storage", class: "" },
+      { list: "Unlimited Public Projects", class: "" },
+      { list: "Community Access", class: "" },
+      { list: "Unlimited Private Projects", class: "text-muted" },
+      { list: "Dedicated Phone Support", class: "text-muted" },
+      { list: "Free Subdomain", class: "text-muted" },
+      { list: "Monthly Status Reports", class: "text-muted" },
+    ]
+  },
+  {
+    title: "Plus",
+    price: "$9",
+    feature: [
+      { list: "Unlimited User", class: "" },
+      { list: "50GB Storage", class: "" },
+      { list: "Unlimited Public Projects", class: "" },
+      { list: "Community Access", class: "" },
+      { list: "Unlimited Private Projects", class: "" },
+      { list: "Dedicated Phone Support", class: "" },
+      { list: "Free Subdomain", class: "" },
+      { list: "Monthly Status Reports", class: "text-muted" },
+    ]
+  },
+  {
+    title: "Pro",
+    price: "$49",
+    feature: [
+      { list: "Unlimited User", class: "" },
+      { list: "150GB Storage", class: "" },
+      { list: "Unlimited Public Projects", class: "" },
+      { list: "Community Access", class: "" },
+      { list: "Unlimited Private Projects", class: "" },
+      { list: "Dedicated Phone Support", class: "" },
+      { list: "Unlimited Free Subdomain", class: "" },
+      { list: "Monthly Status Reports", class: "" },
+    ]
+  },
+];
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="pricing py-5">
+      <div className="container">
+        <div className="row">
+          {details.map((detail) => {
+            return (
+              <Card
+                title={detail.title}
+                price={detail.price}
+                feature={detail.feature}
+              ></Card>
+            );
+          })}
+        </div>
+      </div>
+    </section>
   );
 }
 
